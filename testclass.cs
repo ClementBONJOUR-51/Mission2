ff1142127c4cc64a0b883df947cfde417b661675
Clément _ <bonjourclement51100@gmail.com>
2020-03-14 15:46:01 +0100

    tests unitaires ok

--- testclass.cs
+++ testclass.cs
@@ -1,15 +1,24 @@
 using Xunit;
+using System;
 
-public class testclass
+namespace Mission2
 {
-    [Fact]
-    public void TestName()
+    public class testclass
     {
-        //Given
+        [Fact]
+        public void testGetMoisPrecedent()
+        {
+            DateTime dateTest = new DateTime(2020, 03, 14);
+            Assert.Equal("02", gestionDate.getMoisPrecedent(dateTest));
+            Assert.Equal("02", gestionDate.getMoisPrecedent()); //ici mettre le mois précedant du mois actuel ("02" actuellement)
+        }
 
-        //When
-
-        //Then
-        Assert.Equal(2, 2);
+        [Fact]
+        public void testGetMoisSuivant()
+        {
+            DateTime dateTest = new DateTime(2020, 03, 14);
+            Assert.Equal("04", gestionDate.getMoisSuivant(dateTest));
+            Assert.Equal("04", gestionDate.getMoisSuivant()); //ici mettre le mois suivant du mois actuel ("02" actuellement)
+        }
     }
 }
