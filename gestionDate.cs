e6f8677593abf9ef914e04ad5db697df79bbdcb8
Clément _ <bonjourclement51100@gmail.com>
2020-03-14 16:22:46 +0100

    fonction entre  testUnit ok

--- gestionDate.cs
+++ gestionDate.cs
@@ -28,5 +28,19 @@ namespace Mission2
             DateTime reponse = date.AddMonths(1);
             return reponse.ToString("MM");
         }
+
+        public static Boolean entre(int date1, int date2){
+            DateTime actu = DateTime.Now;
+            if(date1==date2){return actu.Day == date1;}
+            else if(date1<date2){return actu.Day>=date1 && actu.Day<=date2;}
+            else{return actu.Day>=date2 && actu.Day<=date1;}
+        }
+
+        public static Boolean entre(int date1, int date2,DateTime date){
+            DateTime actu = date;
+            if(date1==date2){return actu.Day == date1;}
+            else if(date1<date2){return actu.Day>=date1 && actu.Day<=date2;}
+            else{return actu.Day>=date2 && actu.Day<=date1;}
+        }
     }
 }
--- testclass.cs
+++ testclass.cs
@@ -18,7 +18,40 @@ namespace Mission2
         {
             DateTime dateTest = new DateTime(2020, 03, 14);
             Assert.Equal("04", gestionDate.getMoisSuivant(dateTest));
-            Assert.Equal("04", gestionDate.getMoisSuivant()); //ici mettre le mois suivant du mois actuel ("02" actuellement)
+            Assert.Equal("04", gestionDate.getMoisSuivant()); //ici mettre le mois suivant du mois actuel ("14/03/2020")
+        }
+
+                [Fact]
+        public void testEntre()
+        {
+            DateTime dateTest = new DateTime(2020,03,14);
+            //2 param
+            Assert.Equal(true, gestionDate.entre(10,20)); // ("14/03/2020")
+            Assert.Equal(true, gestionDate.entre(20,10)); // ("14/03/2020")
+
+            Assert.Equal(false, gestionDate.entre(20,30)); // ("14/03/2020")
+            Assert.Equal(false, gestionDate.entre(30,20)); // ("14/03/2020")
+
+            Assert.Equal(true, gestionDate.entre(14,20)); // ("14/03/2020")
+            Assert.Equal(true, gestionDate.entre(20,14)); // ("14/03/2020")
+            Assert.Equal(true, gestionDate.entre(10,14)); // ("14/03/2020")
+            Assert.Equal(true, gestionDate.entre(14,10)); // ("14/03/2020")
+
+            Assert.Equal(true, gestionDate.entre(14,14)); // ("14/03/2020")
+
+            //3 param
+            Assert.Equal(true, gestionDate.entre(10,20,dateTest));
+            Assert.Equal(true, gestionDate.entre(20,10,dateTest));
+
+            Assert.Equal(false, gestionDate.entre(20,30,dateTest));
+            Assert.Equal(false, gestionDate.entre(30,20,dateTest));
+
+            Assert.Equal(true, gestionDate.entre(14,20,dateTest));
+            Assert.Equal(true, gestionDate.entre(20,14,dateTest));
+            Assert.Equal(true, gestionDate.entre(10,14,dateTest));
+            Assert.Equal(true, gestionDate.entre(14,10,dateTest));
+
+            Assert.Equal(true, gestionDate.entre(14,14,dateTest));
         }
     }
 }
